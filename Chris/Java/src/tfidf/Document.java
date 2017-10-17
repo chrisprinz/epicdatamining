223086a73b10d482c32ffd975013cab69e7748e1
Christian Steinmeyer <christian.steinmeyer.1993@gmail.com>
2017-10-17 14:46:13 +0200

    replaced simple exercise file by repository implementation
    also added document implementation

--- Chris/Java/src/tfidf/Document.java
+++ Chris/Java/src/tfidf/Document.java
@@ -12,8 +12,12 @@ import java.util.Map;
 
 public class Document {
     private Map<String, Double> wordCounts = new HashMap<>();
+    private double totalNumberOfWords = 0;
+    private String mostFrequentWord;
+    private double occurrencesOfMostFrequentWord = 0;
 
     public Document(String filename) {
+        super();
         try (BufferedReader reader = new BufferedReader(
                 new InputStreamReader(new FileInputStream(filename), Charset.forName("UTF-8")))) {
             String line;
@@ -23,16 +27,30 @@ public class Document {
                 words.forEach(word -> {
                     if (wordCounts.containsKey(word)) {
                         double oldCount = wordCounts.get(word);
+                        if (oldCount + 1 > occurrencesOfMostFrequentWord) {
+                            occurrencesOfMostFrequentWord = oldCount + 1;
+                            mostFrequentWord = word;
+                        }
                         wordCounts.put(word, oldCount + 1);
                     } else {
                         wordCounts.put(word, 1.0);
                     }
+
+                    totalNumberOfWords++;
                 });
             }
         } catch (IOException e) {
             e.printStackTrace();
             System.err.println(e.getLocalizedMessage());
         }
+    }
+
+    public boolean contains(String term){
+        return wordCounts.get(term) != null;
+    }
+
+    public double tf(String term) {
+        return (wordCounts.get(term) / totalNumberOfWords) / (occurrencesOfMostFrequentWord / totalNumberOfWords);
     }
 
     private String trim(String string) {
---
+++ Chris/Java/src/tfidf/Repository.java
@@ -0,0 +1,27 @@
+package tfidf;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class Repository {
+
+    Collection<Document> documents = new HashSet<>();
+
+    Repository(Collection<String> filenames) {
+        super();
+        filenames.forEach(filename -> documents.add(new Document(filename)));
+    }
+
+    public double idf(String term) {
+        double numberOfDocumentsContainingTerm =
+                documents.parallelStream().filter(document -> document.contains(term)).count();
+        double totalNumberOfDocuments = documents.size();
+        return Math.log(totalNumberOfDocuments / numberOfDocumentsContainingTerm);
+    }
+
+    public double tfIdf(String term, Document document) {
+        double tf = document.tf(term);
+        double idf = idf(term);
+        return tf * idf;
+    }
+}
