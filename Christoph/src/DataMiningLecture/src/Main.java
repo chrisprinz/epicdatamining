18a6d7ffbba620bb1b6abaf9cd791b0dfd3030bf
Christoph Prinz <cprinz@posteo.de>
2017-10-19 09:25:33 +0200

    adds idf calculation

--- Christoph/src/DataMiningLecture/src/Main.java
+++ Christoph/src/DataMiningLecture/src/Main.java
@@ -1,13 +1,19 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Integer.toString(get_idf(10000000, 40)));
-        System.out.println(Integer.toString(get_idf(10000000, 10000)));
+        System.out.println("Exercise 1.2a:");
+        System.out.println(Integer.toString((int)Math.round(get_idf(10000000, 40))));
+        System.out.println(Integer.toString((int)Math.round(get_idf(10000000, 10000))));
     }
 
-    private static int get_idf(double N, double n)
+    /**
+     * Method to calculate inverse document frequency
+     * @param N: Total number of Words
+     * @param n_i: Number of occurrences of a term
+     * @return value for idf
+     */
+    private static double get_idf(double N, double n_i)
     {
-        double result = Math.log(N/n);
-        return (int)Math.round(result);
+        return Math.log(N/n_i);
     }
 }
