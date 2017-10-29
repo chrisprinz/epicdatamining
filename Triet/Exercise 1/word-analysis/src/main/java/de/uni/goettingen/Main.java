60e502df75efdff9e27657f81b3c410b96cd73ba
Triet Doan <doanhoanhtriet@gmail.com>
2017-10-29 22:20:46 +0100

    Put data to HDFS.

--- Triet/Exercise 1/word-analysis/src/main/java/de/uni/goettingen/Main.java
+++ Triet/Exercise 1/word-analysis/src/main/java/de/uni/goettingen/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
         TFIDFCalculator calculator = new TFIDFCalculator();
 
+        calculator.readData();
         calculator.calculateTFIDF();
     }
 }
--- Triet/Exercise 1/word-analysis/src/main/java/de/uni/goettingen/TFIDFCalculator.java
+++ Triet/Exercise 1/word-analysis/src/main/java/de/uni/goettingen/TFIDFCalculator.java
@@ -3,6 +3,8 @@ package de.uni.goettingen;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.Text;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.mahout.common.Pair;
 import org.apache.mahout.vectorizer.DictionaryVectorizer;
@@ -10,8 +12,12 @@ import org.apache.mahout.vectorizer.DocumentProcessor;
 import org.apache.mahout.vectorizer.common.PartialVectorMerger;
 import org.apache.mahout.vectorizer.tfidf.TFIDFConverter;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class TFIDFCalculator {
 
@@ -28,10 +34,34 @@ public class TFIDFCalculator {
         fileSystem = FileSystem.get(configuration);
 
         outputFolder = "output/";
-        documentsSequencePath = new Path("data");
+        documentsSequencePath = new Path(outputFolder, "sequence");
         tokenizedDocumentsPath = new Path(outputFolder, DocumentProcessor.TOKENIZED_DOCUMENT_OUTPUT_FOLDER);
         tfidfPath = new Path(outputFolder + "tfidf");
         termFrequencyVectorsPath = new Path(outputFolder + DictionaryVectorizer.DOCUMENT_VECTOR_OUTPUT_FOLDER);
+    }
+
+    public void readData() throws IOException {
+        File folder = new File("data");
+        File[] files = folder.listFiles();
+        if (files == null) {
+            System.out.println("The directory is empty!");
+            return;
+        }
+
+        try (SequenceFile.Writer writer = new SequenceFile.Writer(fileSystem, configuration, documentsSequencePath, Text.class, Text.class)) {
+            for (File file : files) {
+                Text id = new Text(file.getName());
+
+                StringBuilder builder = new StringBuilder();
+                try (Stream<String> stream = Files.lines(Paths.get(file.getPath()))) {
+                    stream.forEach(builder::append);
+                }
+                Text content = new Text(builder.toString());
+                writer.append(id, content);
+            }
+            writer.close();
+        }
+        System.out.println("Read data successfully!");
     }
 
     public void calculateTFIDF() throws InterruptedException, IOException, ClassNotFoundException {
@@ -53,5 +83,7 @@ public class TFIDFCalculator {
         TFIDFConverter.processTfIdf(termFrequencyVectorsPath, tfidfPath,
                 configuration, documentFrequencies, 1, 100,
                 PartialVectorMerger.NO_NORMALIZING, false, false, false, 1);
+
+        System.out.println("The calculation has finished successfully!");
     }
 }
