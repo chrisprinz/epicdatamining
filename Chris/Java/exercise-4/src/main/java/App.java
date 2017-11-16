04a801ef66f939db051299d4b472b87a7d86405c
Christian Steinmeyer <christian.steinmeyer.1993@gmail.com>
2017-11-16 22:49:32 +0100

    integrated Matrix data type to implement a, b

--- Chris/Java/exercise-4/src/main/java/App.java
+++ Chris/Java/exercise-4/src/main/java/App.java
@@ -1,39 +1,52 @@
 import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
-import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
-import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
-import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
-import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
-import org.apache.mahout.cf.taste.recommender.RecommendedItem;
-import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
-import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
-import java.io.*;
-import java.net.URL;
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
 
 public class App {
+
 
     public static void main(String[] args) {
 
         try {
+            DataModel data = getDataModelFromFile();
+            Matrix matrix = getUserItemCharacteristicMatrix(data);
+            matrix = fillUserItemMatrix(data, matrix);
+            System.out.print(matrix);
 
-            File file = new File("data/data.txt");
-            // load data
-            DataModel data = new FileDataModel(file);
-
-            UserSimilarity similarity = new PearsonCorrelationSimilarity(data);
-
-            UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, data);
-
-            UserBasedRecommender recommender = new GenericUserBasedRecommender(data, neighborhood, similarity);
-
-            List<RecommendedItem> recommendations = recommender.recommend(2, 3);
-            recommendations.forEach(System.out::println);
         } catch (TasteException | IOException e) {
             e.printStackTrace();
         }
 
+    }
+
+    private static Matrix fillUserItemMatrix(DataModel data, Matrix matrix) throws TasteException {
+        data.getUserIDs().forEachRemaining(user -> {
+            try {
+                data.getItemIDsFromUser(user).forEach(item -> matrix.addCharacteristic(user, item));
+            } catch (TasteException e) {
+                e.printStackTrace();
+            }
+        });
+        return matrix;
+    }
+
+    private static Matrix getUserItemCharacteristicMatrix(DataModel data) throws TasteException {
+        LinkedList<Long> userIDs = new LinkedList<>();
+        data.getUserIDs().forEachRemaining(userIDs::add);
+        userIDs.sort(Long::compareTo);
+        LinkedList<Long> itemIDs = new LinkedList<>();
+        data.getItemIDs().forEachRemaining(itemIDs::add);
+        itemIDs.sort(Long::compareTo);
+        return new Matrix(userIDs, itemIDs);
+    }
+
+    private static DataModel getDataModelFromFile() throws IOException {
+        String filename = "data/data.txt";
+        File file = new File(filename);
+        return new FileDataModel(file);
     }
 }
