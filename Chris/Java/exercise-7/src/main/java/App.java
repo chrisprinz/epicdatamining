e355c97312bf452f2bef6ff9ad1466fd114b270c
Christian Steinmeyer <christian.steinmeyer.1993@gmail.com>
2018-01-22 16:31:14 +0100

    added new app implementation and recommender interface

--- Chris/Java/exercise-7/src/main/java/App.java
+++ Chris/Java/exercise-7/src/main/java/App.java
@@ -1,101 +1,39 @@
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.SequenceFile;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.Writable;
-import org.apache.mahout.clustering.canopy.CanopyDriver;
-import org.apache.mahout.clustering.fuzzykmeans.FuzzyKMeansDriver;
-import org.apache.mahout.common.distance.EuclideanDistanceMeasure;
-import org.apache.mahout.common.iterator.sequencefile.SequenceFileIterable;
-import org.apache.mahout.math.DenseVector;
-import org.apache.mahout.math.VectorWritable;
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.model.DataModel;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.File;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 public class App {
 
-    private static final String[] filenames = {"book_example_data.txt", "iris_data.txt"};
     private static final String DATA_DIRECTORY = "data/";
-    private static final String TARGET_DIRECTORY = "target/";
-    private static final String VECTOR_DIRECTORY = TARGET_DIRECTORY + "vectors/";
-    private static final String CENTROID_DIRECTORY = TARGET_DIRECTORY + "centroids/";
-    private static final String CLUSTER_DIRECTORY = TARGET_DIRECTORY + "clusters/";
+    private static final String FILE_NAME = "netflix_data.txt";
+    private static final long[] USER_IDs = {2238060, 1300759, 2143500}; // some users with > 20 movies
+    private static final int NUMBER_OF_RECOMMENDATIONS = 4;
 
-    private static Configuration configuration = new Configuration();
-    private static FileSystem fileSystem;
+    private static List<MyRecommender> recommenders = new LinkedList<>();
 
     public static void main(String[] args) {
         try {
-            System.out.println("\n\n\nInitializing\n\n\n");
             initialize();
-            for (String filename : filenames) {
-                readDocument(filename);
-                cleanFileSystem(filename);
-                performClustering(filename);
-                printSequenceFile(filename);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (IllegalStateException | NullPointerException e) {
-            System.err.println(e.getMessage());
-        }
-    }
-
-
-    private static void initialize() throws IOException {
-        fileSystem = FileSystem.get(configuration);
-    }
-
-    private static void cleanFileSystem(String fullFilename) throws IOException {
-        String filename = fullFilename.substring(0, fullFilename.lastIndexOf("."));
-        Path oldClusterPath = new Path(CLUSTER_DIRECTORY, filename);
-        if (fileSystem.exists(oldClusterPath)) {
-            fileSystem.delete(oldClusterPath, true);
-        }
-    }
-
-    private static void readDocument(String fullFilename) {
-        String filename = fullFilename.substring(0, fullFilename.lastIndexOf("."));
-        try (BufferedReader reader = new BufferedReader(new FileReader(DATA_DIRECTORY + fullFilename));
-             SequenceFile.Writer writer = new SequenceFile.Writer(fileSystem,
-                     configuration, new Path(VECTOR_DIRECTORY, filename), Text.class, VectorWritable.class)) {
-            long id = 0;
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] elements = line.split(",");
-                double[] values = new double[elements.length];
-                for (int i = 0; i < elements.length; i++) {
-                    values[i] = Double.parseDouble(elements[i]);
+            for(MyRecommender recommender: recommenders) {
+                for(long user: USER_IDs){
+                    recommender.printRecommendations(user, NUMBER_OF_RECOMMENDATIONS);
                 }
-                DenseVector vector = new DenseVector(values);
-                writer.append(new Text(String.valueOf(id)), new VectorWritable(vector));
+                recommender.printStatistics();
             }
-        } catch (IOException e) {
+        } catch (TasteException | IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static void performClustering(String fullFilename) {
-        String filename = fullFilename.substring(0, fullFilename.lastIndexOf("."));
-        try {
-            CanopyDriver.run(new Path(VECTOR_DIRECTORY, filename), new Path(CENTROID_DIRECTORY, filename),
-                    new EuclideanDistanceMeasure(), 20, 5, true, 0, true);
 
-            FuzzyKMeansDriver.run(new Path(VECTOR_DIRECTORY, filename), new Path(CENTROID_DIRECTORY, filename+"/clusters-0-final"),
-                    new Path(CLUSTER_DIRECTORY, filename), 0.01, 20, 2, true, true, 0, false);
-        } catch (IOException | InterruptedException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+    private static void initialize() throws IOException, TasteException {
+        DataModel model = new NetflixFileDataModel(new File(DATA_DIRECTORY + FILE_NAME));
+        recommenders.add(new MySVDPlusPlusRecommender(model));
+        recommenders.add(new MySVDALSWRRecommender(model));
+        recommenders.add(new MyUserBasedRecommender(model));
     }
-
-    private static void printSequenceFile(String fullFilename) {
-        String filename = fullFilename.substring(0, fullFilename.lastIndexOf("."));
-        SequenceFileIterable<Writable, Writable> iterable =
-                new SequenceFileIterable<>(new Path(CLUSTER_DIRECTORY, filename+"/clusteredPoints/part-m-00000"), configuration);
-        iterable.forEach(pair -> System.out.format("%10s -> %s\n", pair.getFirst(), pair.getSecond()));
-    }
-
 }
